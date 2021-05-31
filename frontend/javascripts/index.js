7c6db96ef357f19721ed5a385944eff29541b459
Michael Martinez <zeroeclipse@live.com>
2021-05-30 23:46:49 -0700

    Starting setting up the gameboard window.

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,28 +1,36 @@
 const header1 = () => document.getElementById("header1")
 const startButton = () => document.getElementById("start-button")
-const heroCollection = () => document.getElementById("hero-collection")
-
+const canvas = () => document.getElementById("canvas")
+const context = () => canvas.getContext('2d');
+// const heroCollection = () => document.getElementById("hero-collection")
+const cell = 100;
+const gameBoard = [];
 document.addEventListener("DOMContentLoaded", () => {
-    startButton().addEventListener("click", handleClick)
+    startButton().addEventListener("click", handleShowBoard)
 })
 
-const handleClick = () => {
-    fetch('http://localhost:3000/heros')
-    .then(response => response.json())
-    .then(json => renderHeroCollection(json))
-    .catch(err => console.log(err))
+const handleShowBoard = () => {
+    canvas.width = 1000;
+    canvas.height = 600;
 }
-const renderHeroCollection = (jsonData) => {
-    jsonData.forEach(element => {
-        const li = document.createElement("li")
-        li.innerHTML = `
-        <h1 class="hero-name">${element.name}</h1>
-        <h2 class="hero-level">Level: ${element.level}</h2>
-        <p class="hero-experience">Exp: ${element.experience} </p>
-        <p class="hero-attack">Attack: ${element.attack} </p>
-        <p class="hero-defense">Defense: ${element.defense} </p>
-        `
-        heroCollection().appendChild(li)
 
-    })
-}
+// const handleShowHerosClick = () => {
+//     fetch('http://localhost:3000/heros')
+//     .then(response => response.json())
+//     .then(json => renderHeroCollection(json))
+//     .catch(err => console.log(err))
+// }
+// const renderHeroCollection = (jsonData) => {
+//     jsonData.forEach(element => {
+//         const li = document.createElement("li")
+//         li.innerHTML = `
+//         <h1 class="hero-name">${element.name}</h1>
+//         <h2 class="hero-level">Level: ${element.level}</h2>
+//         <p class="hero-experience">Exp: ${element.experience} </p>
+//         <p class="hero-attack">Attack: ${element.attack} </p>
+//         <p class="hero-defense">Defense: ${element.defense} </p>
+//         `
+//         heroCollection().appendChild(li)
+
+//     })
+// }
